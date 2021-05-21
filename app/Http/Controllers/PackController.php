43474838676af14015aacca838f508449d9ea3af
panjezor <filipjancieslik@gmail.com>
2021-05-21 23:51:18 +0100

    initial commit

---
+++ app/Http/Controllers/PackController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pack;
+use Illuminate\Http\Request;
+
+class PackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Pack  $pack
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Pack $pack)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Pack  $pack
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Pack $pack)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Pack  $pack
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Pack $pack)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Pack  $pack
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Pack $pack)
+    {
+        //
+    }
+}
---
+++ database/seeders/PackSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PackSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
